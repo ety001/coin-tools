f8979857473a1b28140a664ca888956f9d020ff8
ety001 <ety001@users.noreply.github.com>
2018-05-01 04:07:17 +0800

    Update run.sh

--- detect-steem-witness/run.sh
+++ detect-steem-witness/run.sh
@@ -13,11 +13,12 @@ else
 fi
 
 logs=`docker logs --tail=1 $containerName`
-regx=/[ [:alnum:] ]+ms.+th_a.+application.cpp:514.+handle_block.+].+Got [ [:digit:] ]+ transactions on block [ [:digit:] ]+ by [ [:alnum:] ]+ -- latency: [ [:digit:] ]+ms$/
+regx='[[:alnum:]]+ms.+th_a.+application.cpp:514.+handle_block.+].+Got [[:digit:]]+ transactions on block [[:digit:]]+ by [[:alnum:]]+ -- latency: [[:digit:]]+ms'
 
-if [[ "$logs" =~ $regx ]];
+if [[ $logs =~ $regx ]];
 then
-    echo "ok"
+    echo 'ok'
+    # curl -d "content=[ok]:smiley: $logs" "$notifyURL"
 else
     curl -d "content=[err]:skull: $logs" "$notifyURL"
 fi
